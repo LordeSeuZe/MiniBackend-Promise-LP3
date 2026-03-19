78456b7f02430a52c6dc5485be977ff1e58a0563
Rafael Cacho <110058677+LordeSeuZe@users.noreply.github.com>
2026-03-18 21:35:10 -0400

    Update userController.js

--- src/js/userController.js
+++ src/js/userController.js
@@ -1,6 +1,6 @@
 import obterUsuario from "./userService.js";
 
-export async function buscarUsuarioController(id) {
+export default async function buscarUsuarioController(id) {
     try {
         const resultBusca = await obterUsuario(id);
         console.log(resultBusca);
